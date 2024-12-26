3d158586bec99eb7440dfcaf1c21db27265d8b27
chbsaikiran <sai.406@gmail.com>
2024-12-26 18:45:55 +0530

    updated .sh so that log are printed on screen and also stored in .txt file

--- copy_and_unzip.sh
+++ copy_and_unzip.sh
@@ -5,5 +5,5 @@ cd data
 unzip imagenet-object-localization-challenge.zip
 cd ..
 python convert_val.py -d ./data/ILSVRC/Data/CLS-LOC/val -l ./data/LOC_val_solution.csv
-python train.py --model resnet50 --data-path ./data/ILSVRC/Data/CLS-LOC --batch-size 256 --epochs 100 --print-freq 100 --lr 0.1 --opt sgd --weight-decay 1e-4 --lr-scheduler cosineannealinglr --lr-warmup-epochs 5 --lr-warmup-method linear --output-dir ./output --amp --mixup-alpha 0.2 --cutmix-alpha 1.0 --auto-augment ra --label-smoothing 0.1 >> log.txt
+python train.py --model resnet50 --data-path ./data/ILSVRC/Data/CLS-LOC --batch-size 256 --epochs 100 --print-freq 100 --lr 0.1 --opt sgd --weight-decay 1e-4 --lr-scheduler cosineannealinglr --lr-warmup-epochs 5 --lr-warmup-method linear --output-dir ./output --amp --mixup-alpha 0.2 --cutmix-alpha 1.0 --auto-augment ra --label-smoothing 0.1 | tee log.txt
 
