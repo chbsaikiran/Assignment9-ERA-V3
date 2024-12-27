24c67b1d87282233595691ec2789cadf3f718448
chbsaikiran <sai.406@gmail.com>
2024-12-27 15:49:32 +0530

    updated .sh and .ipynb files

--- copy_and_unzip.sh
+++ copy_and_unzip.sh
@@ -5,5 +5,5 @@ cd data
 unzip imagenet-object-localization-challenge.zip
 cd ..
 python convert_val.py -d ./data/ILSVRC/Data/CLS-LOC/val -l ./data/LOC_val_solution.csv
-python train_lightning.py --model resnet50 --data-path ./data/ILSVRC/Data/CLS-LOC --batch-size 256 --epochs 100 --lr 0.1 --opt sgd --weight-decay 1e-4 --lr-scheduler cosineannealinglr --lr-warmup-epochs 5 --lr-warmup-method linear --output-dir ./output --amp --mixup-alpha 0.2 --cutmix-alpha 1.0 --auto-augment ra --label-smoothing 0.1 | tee log.txt
+python train_lightning.py --model resnet50 --data-path ./data/ILSVRC/Data/CLS-LOC --batch-size 256 --epochs 100 --lr 0.1 --opt sgd --weight-decay 1e-4 --lr-scheduler cosineannealinglr --lr-warmup-epochs 5 --lr-warmup-method linear --output-dir ./output --amp --mixup-alpha 0.2 --cutmix-alpha 1.0 --auto-augment ra --label-smoothing 0.1 --load-checkpoint ./output/model_start.pth | tee log.txt
 
